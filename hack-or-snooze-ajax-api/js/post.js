459423a411b5e9166bc6fdd7aa98097dce8bbbe0
RTaylorAshka <121143279+RTaylorAshka@users.noreply.github.com>
2023-02-16 02:29:55 -0800

    Add files via upload

---
+++ hack-or-snooze-ajax-api/js/post.js
@@ -0,0 +1,40 @@
+"use strict";
+
+$postTab.on("click", postClick);
+
+function postClick(evt){
+    console.log(evt);
+    hidePageComponents();
+    $postFormPage.slideDown();
+    $favTab.addClass("dimmed");
+    $postTab.removeClass("dimmed");
+    $storyTab.addClass("dimmed");
+
+
+}
+
+$postButton.on("click", postSubmit);
+
+function postSubmit(){
+    const $postVals = $("#post-form > input[type=text], input[type=url]");
+    const storySubmit = 
+    {   title: $postVals[0].value, 
+        author: $postVals[1].value, 
+        url: $postVals[2].value
+    }
+    console.log(storySubmit);
+    storyList.addStory(currentUser,
+        {   title: storySubmit.title, 
+            author: storySubmit.author, 
+            url: storySubmit.url
+        });
+    
+    alert("Post submitted!");
+    start();
+    
+
+
+
+    
+}
+
